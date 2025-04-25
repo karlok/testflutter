fca3af4b141abec875310ce961f0691e4ceabf23
karlo kilayko <kkilayko@wolfko.com>
2025-04-25 14:47:16 -0400

    added awesome flutter thing - Hero anim. very cool

--- lib/models/pokemon.dart
+++ lib/models/pokemon.dart
@@ -1,10 +1,22 @@
 class Pokemon {
   final String name;
   final String url; // check the API docs
+  final int id;
+  final String imageUrl;
 
-  Pokemon({required this.name, required this.url});
+  Pokemon({required this.name, required this.url})
+      : id = _extractId(url),
+        imageUrl =
+            'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${_extractId(url)}.png';
 
   factory Pokemon.fromJson(Map<String, dynamic> json) {
     return Pokemon(name: json['name'], url: json['url']);
+  }
+
+  // get the specific pokemon id int, as this will be used for detail and image
+  static int _extractId(String url) {
+    final uri = Uri.parse(url);
+    final segments = uri.pathSegments;
+    return int.parse(segments[segments.length - 2]);
   }
 }
--- lib/widgets/pokemon_card.dart
+++ lib/widgets/pokemon_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import '../models/pokemon.dart';
+import '../views/pokemon_detail_view.dart';
 
 class PokemonCard extends StatelessWidget {
   final Pokemon pokemon;
@@ -18,26 +19,24 @@ class PokemonCard extends StatelessWidget {
           ),
         );
       },
-    child Card(
-      elevation: 4,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
+      child: Card(
+        elevation: 4,
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
           Hero(
+            // does the nifty anim from List to Detail - very cool!
             tag: pokemon.name,
-            child: CachedNetworkImage( // loads and caches image
-              imageUrl: pokemon.imageUrl,
-              height: 100,
-              width: 100,
-              placeholder: (context, url) => CircularProgressIndicator(),
-              errorWidget: (content, url) => Icon(Icons.error)
-            ),
+            child: CachedNetworkImage(
+                imageUrl: pokemon.imageUrl,
+                height: 100,
+                width: 100,
+                placeholder: (context, url) => CircularProgressIndicator(),
+                errorWidget: (content, url, error) => Icon(Icons.error)),
           ),
           SizedBox(height: 8),
-          Text(
-            pokemon.name.toUpperCase(),
-            style: TextStyle(fontWeight: FontWeight.bold))
-        ),
+          Text(pokemon.name.toUpperCase(),
+              style: TextStyle(fontWeight: FontWeight.bold))
+        ]),
+      ),
     );
   }
 }
